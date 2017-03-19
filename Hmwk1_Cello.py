37e7c0e54e60f3d38b591fa47503abd3dc71d07b
gottben <gottbenn@bu.edu>
2017-03-19 16:56:17 -0400

    Finished forward propagation function

--- Hmwk1_Cello.py
+++ Hmwk1_Cello.py
@@ -101,6 +101,8 @@ scores.close()
 gates_in_circuit = []
 table = []
 count = 1
+input_ymax_ymin = []
+a = 0
 for line in open('scores.txt', 'r'):
     if ('Circuit_score' not in line) and (count != 0):
         table += [line]
@@ -112,6 +114,16 @@ for line in open('scores.txt', 'r'):
         index = line.index('Gate')
         gates_in_circuit += [(line[0: index].replace(" ", ""),
                               line[index + 5: index + 15])]
+        a += 1
+    if 'INPUT' in line:
+        try:
+            index = line.index(' : ')
+            input_ymax_ymin += [(gates_in_circuit[a - 1][0],
+                                 line[index + 4: index + 9])]
+        except:
+            pass
+
+input_ymax_ymin = list(set(input_ymax_ymin))
 # get the set of all the gates in the circuit
 # this guarantees I am not accidentally reproducing
 # gates in the circuit.
@@ -168,7 +180,6 @@ for key in dict_of_circuit.keys():
     for line in gate_info:
         if key in line:
             for element in line:
-                print(line)
                 if len(element) == 1:
                     dict_of_circuit[key]['on_threshold'] = element[
                         0]['on_threshold']
@@ -179,3 +190,205 @@ for key in dict_of_circuit.keys():
                     dict_of_circuit[key]['ymin'] = element[1]['value']
                     dict_of_circuit[key]['K'] = element[2]['value']
                     dict_of_circuit[key]['n'] = element[3]['value']
+
+list_of_circ_elements = []
+for key in dict_of_circuit.keys():
+    dict_of_circuit[key]['ID'] = int(dict_of_circuit[key]['ID'])
+    list_of_circ_elements += [(key, dict_of_circuit[key]['ID'])]
+
+ord_list_of_circ_el = sorted(list_of_circ_elements, key=lambda x: x[1])
+
+# this little chunk of code is to add the y_min and y_max values
+# to the input in our dictionary.
+d = {}
+for k, v in input_ymax_ymin:
+    d.setdefault(k, [k]). append(float(v))
+
+b = [v for v in d.values()]
+for item in b:
+    if item[1] > item[2]:
+        dict_of_circuit[item[0]]['ymax'] = item[1]
+        dict_of_circuit[item[0]]['ymin'] = item[2]
+    else:
+        dict_of_circuit[item[0]]['ymax'] = item[2]
+        dict_of_circuit[item[0]]['ymin'] = item[1]
+
+# we are going to do a little bit more preprocessing here in order to
+# convert all the numbers in our dictionary to floats so we can use them
+# in mathematical operations
+
+for key in dict_of_circuit.keys():
+    dict_of_circuit[key]['score'] = float(dict_of_circuit[key]['score'])
+    try:
+        dict_of_circuit[key]['ymax'] = float(dict_of_circuit[key]['ymax'])
+        dict_of_circuit[key]['ymin'] = float(dict_of_circuit[key]['ymin'])
+        dict_of_circuit[key]['Inputs'] = list(
+            map(int, dict_of_circuit[key]['Inputs']))
+        dict_of_circuit[key]['on_threshold'] = float(
+            dict_of_circuit[key]['on_threshold'])
+        dict_of_circuit[key]['K'] = float(dict_of_circuit[key]['K'])
+        dict_of_circuit[key]['off_threshold'] = float(
+            dict_of_circuit[key]['off_threshold'])
+        dict_of_circuit[key]['n'] = float(dict_of_circuit[key]['n'])
+    except:
+        # dummy variable to make the except statement happy.
+        a = 1
+
+
+# ----------- Eveything above this was essentially just preprocessing -
+# ----------- Below this we are attempting to manipulate the circuit --
+# ----------- parameters to see how that effects the ouptut score  ----
+# we will try multiple methods to calculate what happens when we alter
+# a promoter in a specific parameter to
+    # first find the output in the dictionary
+output_gate = []
+for key in dict_of_circuit.keys():
+    if dict_of_circuit[key]['ID'] == 1:
+        output_gate += [key]
+
+
+def circuit_flow(circuit_dict, starting_gate_list):
+    # find the inputs to the gates in the starting_gate_list
+    for gate in starting_gate_list:
+        for key in circuit_dict:
+            try:
+                if (circuit_dict[key]['ID'] in
+                        circuit_dict[gate]['Inputs']):
+                    starting_gate_list += [(gate, key)]
+            except:
+                pass
+        if len(gate) == 2:
+            for key in circuit_dict:
+                try:
+                    if(circuit_dict[key]['ID'] in
+                            circuit_dict[gate[1]]['Inputs']):
+                        starting_gate_list += [(gate[1], key)]
+                except:
+                    pass
+
+circuit_flow(dict_of_circuit, output_gate)
+output_gate = output_gate[1: len(output_gate)]
+
+circ = {}
+for k, v in output_gate:
+    circ.setdefault(k, [k]). append(v)
+
+circ_list = list(map(tuple, circ.values()))
+
+# this adds the IDs to the circuit list:
+for key in dict_of_circuit.keys():
+    for elem in circ_list:
+        if key == elem[0]:
+            element = (dict_of_circuit[key]['ID'], elem)
+            circ_list[circ_list.index(elem)] = element
+
+# Sort the circuit elements from greatest to least.
+circ_list = sorted(circ_list, reverse=True, key=lambda x: x[0])
+
+# now that I have the inputs the next thing to do is to calculate
+# the score of the inputs given the equation. We want to do this
+# for all the combinations of ymax and ymin. this will give us a list
+# that contains all the possible y values of our output promoter in question.
+
+
+def combinations(iterable, r):
+    # combinations('ABCD', 2) --> AB AC AD BC BD CD
+    # combinations(range(4), 3) --> 012 013 023 123
+    pool = tuple(iterable)
+    n = len(pool)
+    if r > n:
+        return
+    indices = list(range(r))
+    yield tuple(pool[i] for i in indices)
+    while True:
+        for i in reversed(range(r)):
+            if indices[i] != i + n - r:
+                break
+        else:
+            return
+        indices[i] += 1
+        for j in range(i + 1, r):
+            indices[j] = indices[j - 1] + 1
+        yield tuple(pool[i] for i in indices)
+
+# these lines of code convert the input_list which is a list of tuples
+# to a regular list so it can be run in the combination function
+# we have the list of tuples to get ride of any elements after the
+# combinations output that represent an input being both high and
+# low at the same time.
+
+# Now I want to calculate the y_min, y_max of the elements
+# based on the equation:
+# y = ymin + (ymax - ymin)/(1.0 + (x/K)^n)
+
+# circuit properties = [("gatename", ('K', value), ('ymin', value) ... ),(...)]
+circuit_properties = [('pTet', ('ymin', 0.001), ('ymax', 4.4)),
+                      ('A1_AmtR', ('ymin', 0.06), ('ymax', 3.8))]
+
+
+def circuit_forward_prop(circuit_properties):
+    global dict_of_circuit
+    global circ_list
+    # update the dict_of_circuit
+    for x in circuit_properties:
+        for y in x:
+            if isinstance(y, tuple):
+                dict_of_circuit[x[0]][y[0]] = y[1]
+    for ele in circ_list:
+        count = 1
+        for item in ele[1]:
+            if item != ele[1][0]:
+                count += 1
+                y_min = dict_of_circuit[ele[1][0]]['ymin']
+                y_max = dict_of_circuit[ele[1][0]]['ymax']
+                k = dict_of_circuit[ele[1][0]]['K']
+                n = dict_of_circuit[ele[1][0]]['n']
+                if count > 2:
+                    input_list += [(dict_of_circuit[item]['ymin'],
+                                    dict_of_circuit[item]['ymax'])]
+                else:
+                    input_list = [(dict_of_circuit[item]['ymin'],
+                                   dict_of_circuit[item]['ymax'])]
+                the_input = [x for t in input_list for x in t]
+                x_list = list(combinations(the_input, int(len(the_input) / 2)))
+                for item in input_list:
+                    if item in x_list:
+                        x_list.remove(item)
+                y_values = []
+                for b_x in x_list:
+                    x = sum(b_x)
+                    y_values += [y_min + (y_max - y_min) / (1 + (x / k)**n)]
+        dict_of_circuit[ele[1][0]]['ymin'] = min(y_values)
+        dict_of_circuit[ele[1][0]]['ymax'] = max(y_values)
+        score = max(y_values) / min(y_values)
+    print(score)
+    print(dict_of_circuit[ele[1][0]]['score'])
+
+circuit_forward_prop(circuit_properties)
+
+
+# I don't know yet how we are going to modify the UCF file to
+# show different values.
+
+# for info in range(23, 44):
+#     # original_UCF[23:44]:
+#     # save all the gate_names in an array.
+#     gate = []
+#     for key in original_UCF[info]:
+#         if key == 'gate_name':
+#             gate += [info[key]]
+#         elif key == 'variables':
+#             gate += [info[key]]
+#         elif key == 'parameters':
+#             gate += [info[key]]
+#     gate_info += [gate]
+
+# the_input = [x for t in input_list for x in t]
+# r = int(len(the_input) / 2)
+# x_list = list(combinations(the_input, r))
+
+# for item in input_list:
+#     if item in x_list:
+#         x_list.remove(item)
+
+# print(x_list)
