158606f3f7ce4979c6a3ce2f40bae4f5ea256c03
gottben <gottbenn@bu.edu>
2017-03-21 16:21:33 -0400

    Cello_scorer.py clean code

--- Cello.py
+++ Cello.py
@@ -4,80 +4,20 @@ import json
 import numpy as np
 from requests.auth import HTTPBasicAuth
 from itertools import zip_longest
-# The first step of this code is to develop a program that can connect to
-# Cello.
 
+
+# Setup the username and password for Cello Authentication
 username = 'gottbenn'
 password = '123456789'
 auth = HTTPBasicAuth(username, password)
-case = 0
-# I want to write a python tool that performs different post and get operations
-# based on the user input of a certain value.
-# This tool should help in writing the UI
 
-# 1st case: A simple curl test:
-if case == 1:
-    url = 'http://cellocad.org:8080'
-    req = requests.get(url, auth=auth)
-    print(req.text)
-elif case == 2:
-    # 2nd case: get a netlist from the cello website.
-    url = 'http://cellocad.org/netsynth'
-    file = './resources/pycello/resources/AND.v'
-    verilog_text = open(file, 'r').read()
-    params = {'verilog_text': verilog_text}
-    req = requests.post(url, auth=auth, params=params)
-elif case == 3:
-    # 3rd case: Design a circuit using Cello
-    url = 'http://cellocad.org/submit'
-    ID = username + '_Circuit'
-    file = './resources/pycello/resources/AND.v'
-    inputs = './resources/pycello/resources/Inputs.txt'
-    outputs = './resources/pycello/resources/Outputs.txt'
-    verilog_text = open(file, 'r').read()
-    inputs_text = open(inputs, 'r').read()
-    outputs_text = open(outputs, 'r').read()
-    params = {'id': ID, 'verilog_text': verilog_text, 'input_promoter_data':
-              inputs_text, 'output_gene_data': outputs_text}
-    req = requests.post(url, auth=auth, params=params)
-    print(req.text)
-elif case == 4:
-    # 4th case: Get list of completed jobs
-    url = 'http://cellocad.org/results'
-    req = requests.get(url, auth=auth)
-    print(req.text)
-elif case == 5:
-    # 5th case: Get a list of result filenames from a job result
-    ID = username + '_Circuit'
-    url = 'http://cellocad.org/results/' + ID
-    req = requests.get(url, auth=auth)
-    print(req.text)
-elif case == 6:
-    # 6th case: Get the contents of a specified file
-    ID = username + '_Circuit'
-    filename = ID + '_A000' + '_logic' + '_circuit.txt'
-    filename2 = ID + '_A000' + '_bionetlist.txt'
-    filename3 = ID + '_inputs.txt'
-    filename4 = ID + '_outputs.txt'
-    url = 'http://cellocad.org/results/' + ID + '/' + filename4
-    req = requests.get(url, auth=auth)
-    new_file = open('out2.txt', 'w')
-    print(req.text, file=new_file)
-    new_file.close()
-
-# Thoughts on what I need to do. I need to connect to Cello feed it a circuit,
-# then from there have it design the circuit. Once we get what the promoters
-# are for each of the circuits we then need to go into the UCF file and modify
-# the values we want to for the circuit to create DNA Engineering.
-
-# First step have cello give us what the promoters are for each circuit
+# ------------------------------------------------#
+#     UCF File processing                       #
+# -------------------------------------------------#
 with open('gottbenn.UCF.json') as initial_UCF:
     original_UCF = json.load(initial_UCF)
 
-print(original_UCF[23:44])
-
-# Lets get all the known promoters in the UCF file, this should make it easier
-# to identify if one of these promoters is being used in the circuit.
+# We are going to grab all the repressor information from the UCF file.
 gate_info = []
 for info in original_UCF[23:44]:
     # save all the gate_names in an array.
@@ -90,8 +30,10 @@ for info in original_UCF[23:44]:
         elif key == 'parameters':
             gate += [info[key]]
     gate_info += [gate]
-# Next step is to look for the promoters that are used in this circuit.
-# For this we will look at getting the contents of the _logic_circuit.txt file.
+
+# ---------------------------------------------------------#
+#       Grab the circuit info from cello             #
+# ---------------------------------------------------------#
 ID = username + '_Circuit'
 filename = ID + '_A000' + '_logic' + '_circuit.txt'
 url = 'http://cellocad.org/results/' + ID + '/' + filename
@@ -100,6 +42,10 @@ scores = open('scores.txt', 'w')
 print(req.text, file=scores)
 scores.close()
 
+
+# ---------------------------------------------------------------#
+#   Process the circuit info into python object       #
+# ---------------------------------------------------------------#
 gates_in_circuit = []
 table = []
 count = 1
@@ -124,18 +70,11 @@ for line in open('scores.txt', 'r'):
                                  line[index + 4: index + 9])]
         except:
             pass
-
 input_ymax_ymin = list(set(input_ymax_ymin))
-# get the set of all the gates in the circuit
-# this guarantees I am not accidentally reproducing
-# gates in the circuit.
-# I need to grap the numerical IDs of each of the inputs and outputs.
-# Note within the list_of_identities the first parameter of a sub_list is
-# the lexical ID of the promoter. The second element is the numerical ID
-# any element after the second is the input into that specific promoter/
-# or any value that comes before the output. With the file format specified
-# it is generally always guaranteed that the first sub_list is the output
-# identity
+
+# ----------------------------------------------------------------#
+#  Get ride of any unnecessary string elements     #
+# ----------------------------------------------------------------#
 list_of_identities = []
 for line in table:
     if 'OUTPUT' in line:
@@ -163,10 +102,9 @@ for line in table:
         identity = identity.replace(",", " ")
         list_of_identities += [identity.split()]
 
-
-# Now for convenience I want to make a dictionary that has
-# all the information we need of the circuit and its elements.
-
+# -----------------------------------------------------------#
+#  Put the circuit info    into a dictionary           #
+# -----------------------------------------------------------#
 dict_of_circuit = {}
 for sub_list in list_of_identities:
     if sub_list[0] not in dict_of_circuit:
@@ -198,10 +136,27 @@ for key in dict_of_circuit.keys():
     dict_of_circuit[key]['ID'] = int(dict_of_circuit[key]['ID'])
     list_of_circ_elements += [(key, dict_of_circuit[key]['ID'])]
 
-# we are going to do a little bit more preprocessing here in order to
-# convert all the numbers in our dictionary to floats so we can use them
-# in mathematical operations
 
+# ------------------------------------------------------#
+# Add the input ymax and ymin to dict       #
+# ------------------------------------------------------#
+
+d = {}
+for k, v in input_ymax_ymin:
+    d.setdefault(k, [k]). append(float(v))
+
+b = [v for v in d.values()]
+for item in b:
+    if item[1] > item[2]:
+        dict_of_circuit[item[0]]['ymax'] = item[1]
+        dict_of_circuit[item[0]]['ymin'] = item[2]
+    else:
+        dict_of_circuit[item[0]]['ymax'] = item[2]
+        dict_of_circuit[item[0]]['ymin'] = item[1]
+
+# ----------------------------------------------------- #
+# Convert string values to float values       #
+# ----------------------------------------------------- #
 for key in dict_of_circuit.keys():
     dict_of_circuit[key]['score'] = float(dict_of_circuit[key]['score'])
     try:
@@ -219,19 +174,10 @@ for key in dict_of_circuit.keys():
         # dummy variable to make the except statement happy.
         a = 1
 
-
-# ----------- Eveything above this was essentially just preprocessing -
-# ----------- Below this we are attempting to manipulate the circuit --
-# ----------- parameters to see how that effects the ouptut score  ----
-# we will try multiple methods to calculate what happens when we alter
-# a promoter in a specific parameter to
-    # first find the output in the dictionary
- # Eveything above this was essentially just preprocessing -
-# ----------- Below this we are attempting to manipulate the circuit --
-# ----------- parameters to see how that effects the ouptut score  ----
-# we will try multiple methods to calculate what happens when we alter
-# a promoter in a specific parameter to
-    # first find the output in the dictionary
+# -----------------------------------------------------------------------------#
+#   This block of code recreates the flow of the circuit          #
+#   from start to fin.                                                                #
+# -----------------------------------------------------------------------------#
 output_gate = []
 for key in dict_of_circuit.keys():
     if dict_of_circuit[key]['ID'] == 1:
@@ -266,6 +212,10 @@ for k, v in output_gate:
 
 circ_list = list(map(tuple, circ.values()))
 
+# -------------------------------------------------------------------------------#
+# We order the elements in the circ_list based on their ID       #
+# -------------------------------------------------------------------------------#
+
 # this adds the IDs to the circuit list:
 for key in dict_of_circuit.keys():
     for elem in circ_list:
@@ -276,10 +226,11 @@ for key in dict_of_circuit.keys():
 # Sort the circuit elements from greatest to least.
 circ_list = sorted(circ_list, reverse=True, key=lambda x: x[0])
 
-# now that I have the inputs the next thing to do is to calculate
-# the score of the inputs given the equation. We want to do this
-# for all the combinations of ymax and ymin. this will give us a list
-# that contains all the possible y values of our output promoter in question.
+# --------------------------------------------------------------------------------#
+# Define a combinations function that we will use later            #
+# This function was taken from the python website as a          #
+# replacement for the itertools library function combos.          #
+# --------------------------------------------------------------------------------#
 
 
 def combinations(iterable, r):
@@ -302,60 +253,64 @@ def combinations(iterable, r):
             indices[j] = indices[j - 1] + 1
         yield tuple(pool[i] for i in indices)
 
-# these lines of code convert the input_list which is a list of tuples
-# to a regular list so it can be run in the combination function
-# we have the list of tuples to get ride of any elements after the
-# combinations output that represent an input being both high and
-# low at the same time.
-
-# Now I want to calculate the y_min, y_max of the elements
-# based on the equation:
-# y = ymin + (ymax - ymin)/(1.0 + (x/K)^n)
-
-# circuit properties = [("gatename", ('K', value), ('ymin', value) ... ),(...)]
-circuit_properties = [('pTet', ('ymin', 0.001), ('ymax', 4.4)),
-                      ('A1_AmtR', ('ymin', 0.06), ('ymax', 3.8))]
+# -------------------------------------------------------------------------------#
+# Function that turns the dictionary of circuit elements into    #
+# a vector of circuit elements                                                   #
+# -------------------------------------------------------------------------------#
 
 
-# Converts the dict of circuit to a linear array of parameters
 def create_parameters_array(dict_of_circuit):
-    # This assumes that the dictionary is in the same order always
-    # According to some random person on StackOverflow, the order is usually the same but not guaranteed so we may want to switch to using ordered dicts?
-    # Or maybe consider changing it to a list of dictionaries b/c the order is guaranteed
-    gates = [{gatename:values} for gatename,values in dict_of_circuit.items() if len(values) > 4] # Get only the gates that are not the initial inputs
-    params = np.array([[gatevalues['n'], gatevalues['K'], gatevalues['ymax'], gatevalues['ymin']] for gate in gates for gatename, gatevalues in gate.items()]) # list of lists for each gate
-    flat_params = params.flatten() # creates linear array
+    gates = [{gatename: values} for gatename, values in dict_of_circuit.items(
+    ) if len(values) > 4]  # Get only the gates that are not the initial inputs
+    params = np.array([[gatevalues['n'], gatevalues['K'], gatevalues['ymax'],
+                        gatevalues['ymin']] for gate in gates for gatename,
+                       gatevalues in gate.items()])
+    flat_params = params.flatten()  # creates linear array
     return flat_params
 
 
-def grouper(iterable, n, fillvalue=None): # From Python Recipes
-    "Collect data into fixed-length chunks or blocks"
-    # grouper('ABCDEFG', 3, 'x') --> ABC DEF Gxx"
-    args = [iter(iterable)] * n
-    return zip_longest(*args, fillvalue=fillvalue)
+# -------------------------------------------------------------------------------#
+# Grouper Function from python recipes                                  #
+# --------------------------------------------------------------------------------#
+# From Python Recipes "Collect data into fixed-length chunks or blocks"
+# grouper('ABCDEFG', 3, 'x') --> ABC DEF Gxx"
+def grouper(iterable, n, fillvalue=None):
 
-# Converts a linear array to [("gatename", ('K', value), ('ymin', value) ... ),(...)] form which can be used to update dictionary
-# This only works if we assume that the dict_of_circuit order is always the same
+    args = [iter(iterable)] * n
+    return(zip_longest(*args, fillvalue=fillvalue))
+
+# --------------------------------------------------------------------------------#
+# Convert the parameter array to array of tuples                    #
+# --------------------------------------------------------------------------------#
+
+
 def convert_parameter_array_to_tuples(params, dict_of_circuit):
-    print(dict_of_circuit)
-    gates = [gatename for gatename,values in dict_of_circuit.items() if len(values) > 4] # Name of gates that aren't the initial input
-    params_per_gate = grouper(params,4) # Groups the linear array into tuples of size 4 ex: [1,2,3,4,5,6,7,8] --> (1,2,3,4), (5,6,7,8)
-    dict_values = [(gatename,) + tuple(zip(['n', 'K', 'ymax', 'ymin'],gate_params)) for gatename, gate_params in zip(gates, params_per_gate)] # Combines gate names w gate parameters
+    gates = [gatename for gatename, values in dict_of_circuit.items() if len(
+        values) > 4]  # Name of gates that aren't the initial input
+    # Groups the linear array into tuples of size 4 ex: [1,2,3,4,5,6,7,8] -->
+    # (1,2,3,4), (5,6,7,8)
+    params_per_gate = grouper(params, 4)
+    dict_values = [(gatename,) + tuple(zip(['n', 'K', 'ymax', 'ymin'],
+                                           gate_params))
+                   for gatename, gate_params in zip(gates, params_per_gate)]
     return dict_values
 
-# The input needs to be the parameters of the circuit
-# circuit_properties = the initial input to the circuit
-def circuit_forward_prop(parameters, circuit_properties):
+# -----------------------------------------------------------------------------------#
+# Recreates the genetic circuit using forward propagation          #
+# -----------------------------------------------------------------------------------#
+
+
+def circuit_forward_prop(parameters):
     global dict_of_circuit
     global circ_list
 
-    parameter_list = convert_parameter_array_to_tuples(parameters,dict_of_circuit) + circuit_properties
+    parameter_list = (convert_parameter_array_to_tuples(parameters,
+                                                        dict_of_circuit))
 
     # update the dict_of_circuit
     for x in parameter_list:
         for y in x:
             if isinstance(y, tuple):
-                print(dict_of_circuit[x[0]][y[0]], y[1])
                 dict_of_circuit[x[0]][y[0]] = y[1]
 
     for ele in circ_list:
@@ -388,21 +343,9 @@ def circuit_forward_prop(parameters, circuit_properties):
     print(score)
     print(dict_of_circuit[ele[1][0]]['score'])
 
+
+# ---------------------------------------------------------------------------#
+# Run the functions that were made previously                  #
+# ---------------------------------------------------------------------------#
 parameters = create_parameters_array(dict_of_circuit)
-circuit_forward_prop(parameters, circuit_properties)
-
-
-# I don't know yet how we are going to modify the UCF file to
-# show different values.
-
-for info in range(23, 44):
-    for item in circuit_properties:
-        if original_UCF[info]['gate_name'] == item[0]:
-            params = original_UCF[info]['parameters']
-            for x in range(0, len(params)):
-                for y in item:
-                    if original_UCF[info]['parameters'][x]['name'] == y[0]:
-                        original_UCF[info]['parameters'][x]['value'] == y[1]
-
-with open('new.UCF.json', 'w') as outfile:
-    json.dump(original_UCF, outfile)
+circuit_forward_prop(parameters)
---
+++ equation_tester.py
@@ -0,0 +1,37 @@
+pt_off = 0.0013
+pt_on = 4.4
+
+pb_off = 0.008
+pb_on = 2.5
+
+S1_min = 0.003
+S1_max = 2.099
+Sn = 2.6
+Sk = 0.04
+
+A1_min = 0.0722
+A1_max = 3.68718
+An = 1.6
+Ak = 0.07
+
+P3_min = 0.02
+P3_max = 6.738
+Pn = 4.2
+Pk = 0.23
+
+
+def equation(x1, x2, ymin, ymax, k, n):
+    y = ymin + (ymax - ymin)/(1 + ((x1 + x2)/k)**n)
+    return(y)
+
+y1 = equation(pt_off, 0, S1_min, S1_max, Sk, Sn)
+y2 = equation(pt_on, 0, S1_min, S1_max, Sk, Sn)
+print(y1, y2)
+y3 = equation(pb_off, 0, A1_min, A1_max, Ak, An)
+y4 = equation(pb_on, 0, A1_min, A1_max, Ak, An)
+print(y3, y4)
+Py1 = equation(y2, y4, P3_min, P3_max, Pk, Pn)
+Py2 = equation(y1, y3, P3_min, P3_max, Pk, Pn)
+score = Py1/Py2
+
+print(score)
